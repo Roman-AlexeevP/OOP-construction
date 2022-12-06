56a89043cab944e59b089c9932fd7b9a7923642f
Roman Alexeev <62597624+Roman-AlexeevP@users.noreply.github.com>
2022-12-06 14:24:14 +0300

    Update inheritance_types_3.py

--- src/inheritance_types_3.py
+++ src/inheritance_types_3.py
@@ -18,3 +18,13 @@ class IntegerValidator(Validator):
     def validate(self, input):
         return isinstance(int, input)
 
+
+class UpdateView:
+    model_name = ""
+    
+    def update(self, fields):
+        get_model(self.model_name).update(fields)
+        
+ # класс с льготным наследованием, где уточняется модель для обновления
+ class UpdateUser(UpdateView):
+    model_name = "user"
